2e0fab837fc898158188c23240ac056ab9ad84ec
Siddhartha Chatterjee <ninja.siddhartha@gmail.com>
2023-02-15 20:24:05 -0800

    switched to react state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+    const [homozygousDominant, setHomozygousDominant ] = useState(0);
+     const [heterozygous, setHeterozygous ] = useState(0);
+      const [homozygousRecessive, setHomozygousRecessive ] = useState(0);
+    function handleSubmit(event) {
+
+        event.preventDefault();
+        const pValue = Number(document.getElementById('pValue').value);
+        const qValue = Number(document.getElementById('qValue').value);
+        const initialPopulationSize = Number(document.getElementById('initialPopulationSize').value);
+        document.getElementById('equation').innerHTML = `\(${pValue}^2+2*${pValue}*${qValue}+${qValue}^2=1\)`
+        let newHomozygousDominant = pValue * pValue;
+        let newHeterozygous = 2 * pValue * qValue;
+        let newHomozygousRecessive = qValue * qValue;
+        setHomozygousDominant(newHomozygousDominant);
+        setHeterozygous(newHeterozygous);
+        setHomozygousRecessive(newHomozygousRecessive);
+        // document.getElementById('homozygousDominant').innerHTML = `Gen 1 population that is homozygous dominant: \(${Math.round(homozygousDominant*initialPopulationSize)}\)`;
+        // document.getElementById('heterozygous').innerHTML = `Gen 1 population that is heterozygous: \(${Math.round(heterozygous*initialPopulationSize)}\)`;
+        // document.getElementById('homozygousRecessive').innerHTML = `Gen 1 population that is homozygous recessive: \(${Math.round(homozygousRecessive*initialPopulationSize)}\)`;
+    
+    
+    
+    
+    }
     return (
         <div className="App">
             <form onSubmit={handleSubmit}>
@@ -12,29 +38,19 @@ function App() {
                 <input type="number" id="initialPopulationSize" step="any"/>
                 <input type="submit" value="Submit" />
             </form>
-            <p id="equation"></p>
-            <p id="homozygousDominant"></p>
-            <p id="heterozygous"></p>
-            <p id="homozygousRecessive"></p>
+            <p id="equation"> </p>
+            
+            <p id="homozygousDominant">
+                Gen 1 population that is homozygous dominant: {homozygousDominant}
+            </p>
+            <p id="heterozygous">
+                Gen 1 population that is heterozygous: {homozygousRecessive}
+            </p>
+            <p id="homozygousRecessive">
+                Gen 1 population that is homozygous recessive: {homozygousRecessive}
+            </p>
         </div>
     );
-}
-function handleSubmit(event) {
-    event.preventDefault();
-    const pValue = Number(document.getElementById('pValue').value);
-    const qValue = Number(document.getElementById('qValue').value);
-    const initialPopulationSize = Number(document.getElementById('initialPopulationSize').value);
-    document.getElementById('equation').innerHTML = `\(${pValue}^2+2*${pValue}*${qValue}+${qValue}^2=1\)`
-    let homozygousDominant = pValue * pValue;
-    let heterozygous = 2 * pValue * qValue;
-    let homozygousRecessive = qValue * qValue;
-    document.getElementById('homozygousDominant').innerHTML = `Gen 1 population that is homozygous dominant: \(${Math.round(homozygousDominant*initialPopulationSize)}\)`;
-    document.getElementById('heterozygous').innerHTML = `Gen 1 population that is heterozygous: \(${Math.round(heterozygous*initialPopulationSize)}\)`;
-    document.getElementById('homozygousRecessive').innerHTML = `Gen 1 population that is homozygous recessive: \(${Math.round(homozygousRecessive*initialPopulationSize)}\)`;
-
-
-
-
 }
 
 export default App;
