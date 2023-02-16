417a92aaecbbe3866c06a1f8ba558eb0cc55d3a8
Arihant Jain <jainarihantmr@gmail.com>
2023-02-15 20:59:50 -0800

    Use mathjax and output # of people in each section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 import './App.css';
-
+import { MathComponent } from "mathjax-react";
 
 function App() {
+    const [pValue, setPValue] = useState(null);
+    const [qValue, setQValue] = useState(null);
+    const [initialPopulationSize, setInitialPopulationSize] = useState(null);
     const [homozygousDominant, setHomozygousDominant ] = useState(0);
-     const [heterozygous, setHeterozygous ] = useState(0);
-      const [homozygousRecessive, setHomozygousRecessive ] = useState(0);
+    const [heterozygous, setHeterozygous ] = useState(0);
+    const [homozygousRecessive, setHomozygousRecessive ] = useState(0);
+    const [equationTex, setEquationTex] = useState(``);
     function handleSubmit(event) {
 
         event.preventDefault();
         const pValue = Number(document.getElementById('pValue').value);
         const qValue = Number(document.getElementById('qValue').value);
         const initialPopulationSize = Number(document.getElementById('initialPopulationSize').value);
-        document.getElementById('equation').innerHTML = `\(${pValue}^2+2*${pValue}*${qValue}+${qValue}^2=1\)`
+        setPValue(pValue);
+        setQValue(qValue);
+        setInitialPopulationSize(initialPopulationSize);
+        setEquationTex(`${pValue}^2+2*${pValue}*${qValue}+${qValue}^2=1`);
         let newHomozygousDominant = pValue * pValue;
         let newHeterozygous = 2 * pValue * qValue;
         let newHomozygousRecessive = qValue * qValue;
@@ -38,16 +45,20 @@ function App() {
                 <input type="number" id="initialPopulationSize" step="any"/>
                 <input type="submit" value="Submit" />
             </form>
-            <p id="equation"> </p>
+
+            <MathComponent tex={equationTex} />
             
             <p id="homozygousDominant">
-                Gen 1 population that is homozygous dominant: {homozygousDominant}
+                Gen 1 population that is <strong>homozygous dominant</strong>:
+                {pValue && initialPopulationSize ? <MathComponent tex={`${pValue}^2\\times${initialPopulationSize}=${(pValue*pValue).toFixed(3)}\\times${initialPopulationSize}=${(pValue*pValue*initialPopulationSize).toFixed(3)}`}/> : ""}
             </p>
             <p id="heterozygous">
-                Gen 1 population that is heterozygous: {homozygousRecessive}
+                Gen 1 population that is <strong>heterozygous</strong>:
+                {pValue && qValue && initialPopulationSize ? <MathComponent tex={`2\\times${pValue}\\times${qValue}\\times${initialPopulationSize}=${(2*pValue*qValue).toFixed(3)}\\times${initialPopulationSize}=${(2*pValue*qValue*initialPopulationSize).toFixed(3)}`}/> : ""}
             </p>
             <p id="homozygousRecessive">
-                Gen 1 population that is homozygous recessive: {homozygousRecessive}
+                Gen 1 population that is <strong>homozygous recessive</strong>:
+                {qValue && initialPopulationSize ? <MathComponent tex={`${qValue}^2\\times${initialPopulationSize}=${(qValue*qValue).toFixed(3)}\\times${initialPopulationSize}=${(qValue*qValue*initialPopulationSize).toFixed(3)}`}/> : ""}
             </p>
         </div>
     );
